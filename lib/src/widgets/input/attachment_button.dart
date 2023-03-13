aa709e760f115dc7f7db21867483143da9ed7aac
timur.khismatullin <timur.khismatullin@umbrellait.com>
2023-03-13 16:07:26 +0300

    Hide badge on material.dart

--- lib/src/widgets/input/attachment_button.dart
+++ lib/src/widgets/input/attachment_button.dart
@@ -1,5 +1,5 @@
 import 'package:badges/badges.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide Badge;
 
 import '../state/inherited_chat_theme.dart';
 import '../state/inherited_l10n.dart';
